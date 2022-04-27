96088396c12278710d558a43f84d4cef18104abf
JoaoVictorSou <soulima.joao@gmail.com>
2022-04-27 17:19:52 -0300

    utilizando slice

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade
 STRING FATIADA (slicing)
 OBS: str[x:y] o x é inclusivo e o y é excludente
 OBS: str são sequências imutáveis
-url_base = url[:26]
-url_parametros = url[27:77]
 '''
 
-parametros = url_parametros.split('&')
-valor_parametros = []
+parametro = 'quantidade'
 
-for parametro in parametros:
-    indice = parametro.find('=')
-    valor_parametros.append(parametro[indice+1:])
+if parametro in url_parametros:
+    index_parametro = url_parametros.find(parametro)
+    index_valor = index_parametro + len(parametro) + 1
+    index_comercial = url_parametros.find('&', index_parametro)
+    valor = ''
 
-print(valor_parametros)
+    if (url_parametros.find('&', index_parametro) == -1):
+        valor = url_parametros[index_valor:]
+    else:
+        valor = url_parametros[index_valor:index_comercial]
+
+    print(valor)
