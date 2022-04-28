449472fd9880575013ec3a4ae1fe0414f6af967f
JoaoVictorSou <soulima.joao@gmail.com>
2022-04-28 06:19:14 -0300

    validação de url, limpeza da url, metodo para pegar apenas a base e metodo para pegar apenas os parametros

--- Extrator_url.py
+++ Extrator_url.py
@@ -1,2 +1,31 @@
-class ExtratorURL:
-    pass
+class ExtratorURL:    
+    def __init__(self, url):
+        self.url = ExtratorURL.sanitiza_url(url)
+        self.valida_url()
+
+    @staticmethod
+    def sanitiza_url(url):
+        return url.strip()
+    
+    def valida_url(self):
+        if self.url == "":
+            raise ValueError('A URL está vazia')
+    
+    def get_url_base(self):
+        url = self.url
+
+        index_simbolo_query = url.find('?')
+
+        return url[:index_simbolo_query]
+    
+    def get_url_params(self):
+        url = self.url
+
+        index_simbolo_query = url.find("?")
+
+        return url[index_simbolo_query+1:]
+
+extrator = ExtratorURL("http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100")
+
+print(extrator.get_url_base())
+print(extrator.get_url_params())
