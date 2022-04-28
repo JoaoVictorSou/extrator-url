9d038d85e1c8614760e8fb0aa985a1ed0b6a6d75
JoaoVictorSou <soulima.joao@gmail.com>
2022-04-28 06:37:50 -0300

    coletando valor específico do parâmetro

--- Extrator_url.py
+++ Extrator_url.py
@@ -25,7 +25,23 @@ class ExtratorURL:
 
         return url[index_simbolo_query+1:]
 
+    def get_valor_parametro(self, nome_parametro):
+        url_parametros = self.get_url_params()
+
+        if nome_parametro in url_parametros:
+            index_nome_parametro = url_parametros.find(nome_parametro)
+            index_valor = index_nome_parametro + len(nome_parametro) + 1
+
+            if '&' in url_parametros[index_nome_parametro:]:
+                index_divisor = url_parametros.find('&', index_nome_parametro)
+
+                return url_parametros[index_valor:index_divisor]
+            else:
+                return url_parametros[index_valor:]
+
+
 extrator = ExtratorURL("http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100")
 
 print(extrator.get_url_base())
 print(extrator.get_url_params())
+print(extrator.get_valor_parametro('moedaOrigem'))
