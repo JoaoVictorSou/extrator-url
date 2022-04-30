4d2dd8892f0646f2cd764af3d370a1a041e23bff
JoaoVictorSou <soulima.joao@gmail.com>
2022-04-30 17:37:24 -0300

    convertendo uma moeda em outra com um valor atualizado e propriedades para a classe

--- conversor_moeda.py
+++ conversor_moeda.py
@@ -1,4 +1,26 @@
+import requests
+
 class ConversorMoeda:
+    # atributo estático, moedas aceitas pelo conversor
+    moedas = ['USD',
+              'BRL']
+
+    def __init__(self, moeda_origem, moeda_destino, quantidade=0):
+        self.__moeda_origem = self.valida_moedas_em_conversor(moeda_origem)
+        self.__moeda_destino = self.valida_moedas_em_conversor(moeda_destino)
+        self.__quantidade = self.valida_quantidade(quantidade)
+
+    # método que irá converter o valor informado nos atributos do objeto
+    def converter(self):
+        # consumo de API para pegar o valor atual das moedas
+        endpoit = 'https://economia.awesomeapi.com.br/json/last/'
+        response = requests.get(f"{endpoit}/{self.__moeda_origem}-{self.__moeda_destino}")
+        conversao = response.json()
+
+        # retorno do valor da conversão da moeda, considerando o valor do cambio e a quantidade imposta
+        return float(conversao[f'{self.__moeda_origem}{self.__moeda_destino}']['ask']) * self.__quantidade
+
+    # método que pode ser utilizado pela própria classe para ver se uma quantia informada por ser entendido como dinheiro
     @staticmethod
     def valida_quantidade(quantidade):
         tipo_quantidade = type(quantidade)
@@ -9,9 +31,26 @@ class ConversorMoeda:
         raise ValueError(
             'erro: quantidade da moeda deve ser um valor numérico maior que zero residente do conjutos dos reais.')
 
-    def __init__(self, moeda_origem, moeda_destino, quantidade=0):
-        self.__moeda_origem = moeda_origem
-        self.__moeda_destino = moeda_destino
-        self.__quantidade = self.valida_quantidade(quantidade)
+    # método que pode ser utilizado pela própria classe para a consulta de um tipo válido no conversor
+    @staticmethod
+    def valida_moedas_em_conversor(moeda):
+        if moeda in ConversorMoeda.moedas:
+            return moeda
 
-converte_real_dolar = ConversorMoeda('real', 'dolar', 1)
+        raise ValueError('moeda escolhida não existe nos tipos do conversor.')
+
+    @property
+    def moeda_origem(self):
+        return self.__moeda_origem
+
+    @property
+    def moeda_destino(self):
+        return self.__moeda_destino
+
+    @property
+    def quantidade(self):
+        return '{:.2f}'.format(self.__quantidade)
+
+    @quantidade.setter
+    def quantidade(self, nova_quantidade):
+        self.__quantidade = self.valida_quantidade(nova_quantidade)
